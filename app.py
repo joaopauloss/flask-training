58ad1d2a28564be2a6b9e68c731df7b0dfe6b095
joaopauloss <jpaulo.em@gmail.com>
2021-02-01 00:17:05 -0300

    Page 2 start

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -12,7 +12,17 @@ def page1():
 
 @app.route('/page2', methods=('POST', 'GET'))
 def page2():
-    return render_template('page2.html')
+
+    if request.method == 'POST':
+        if request.form["submit_button"] == "write":
+            content = request.form['content']
+            return render_template('page2.html', content=content)
+
+        elif request.form["submit_button"] == "read":
+            return render_template('page2.html', content='Read button pressed')
+            
+    else:
+        return render_template('page2.html', content='Nothing to show')
 
 @app.route('/page3',methods=('POST', 'GET'))
 def page3():
